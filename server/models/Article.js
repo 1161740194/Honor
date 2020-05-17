3c4344ee141e92d615416127b114c765b1b083e4
Beater_B <7512631+beater_b@user.noreply.gitee.com>
2020-05-17 18:55:44 +0800

    初始化

---
+++ server/models/Article.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+
+const schame = new mongoose.Schema({
+    title: {
+        type: String
+    },
+    categories: [{
+        type: mongoose.SchemaTypes.ObjectId,
+        ref: 'Category'
+    }],
+    body: {
+        type: String
+    },
+}, {
+    timestamps: true
+})
+
+module.exports = mongoose.model('Article', schame)
