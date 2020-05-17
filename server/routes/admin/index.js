6077f78aa9acc1e1544108a999477b07e98d6e3c
Beater_B <7512631+beater_b@user.noreply.gitee.com>
2020-05-17 21:22:04 +0800

    ok

--- server/routes/admin/index.js
+++ server/routes/admin/index.js
@@ -65,7 +65,7 @@ module.exports = app => {
 
     app.post('/admin/api/upload', authMiddleware(), upload.single('file'), async (req, res) => {
         const file = req.file
-        file.url = `http://localhost:3000/uploads/${file.filename}`
+        file.url = `http://test.apexwq.top/uploads/${file.filename}`
         res.send(file)
     })
 
